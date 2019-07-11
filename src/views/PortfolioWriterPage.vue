e984a72fd653c244d77b13efe8ffc4f4fd3ebb94
heabin <heabin209@gmail.com>
2019-07-11 14:51:50 +0900

    firebase

--- src/views/PortfolioWriterPage.vue
+++ src/views/PortfolioWriterPage.vue
@@ -8,14 +8,14 @@
         <div class="markdown">
           <v-flex xs9>
 
-            <v-text-field label="title"></v-text-field>
-            <v-btn icon v-on:click="posting"><v-icon>send</v-icon></v-btn>
+            <v-text-field label="title" v-model="title"></v-text-field>
+            <v-btn icon @click="posting"><v-icon>send</v-icon></v-btn>
 
             <uploadImg></uploadImg>
 
           </v-flex>
           <v-flex xs10>
-            <markdown-editor></markdown-editor>
+            <markdown-editor v-model="contents"></markdown-editor>
           </v-flex>
         </div>
       </v-layout>
@@ -27,11 +27,19 @@
 <script>
 import ImgBanner from '../components/ImgBanner'
 import uploadImg from '../components/uploadImg'
+import FirebaseService from '@/services/FirebaseService'
 
 export default {
 	name: 'PortfolioWriterPage',
-  data() {
+  data(){
+    title:'';
+    contents: '';
+    img: '';
 
+    return {
+      title:'',
+      contents: ''
+      }
   },
 	components: {
 		ImgBanner,
@@ -39,8 +47,21 @@ export default {
 	},
   methods:{
     posting(event){
+      // component에서 이미지 불러오기
+      var test = document.getElementById('image')
       console.log("posting is clicked!");
-      console.log(event);
+      console.log(test);
+      this.img = test.src;
+      console.log(this.title)
+      console.log(this.contents)
+      console.log(this.img)
+
+
+      //파이어베이스 디비에 넣는 작업
+      FirebaseService.postPortfolio(this.title,this.contents,this.img)
+      //등록 완료 후, 포트폴리오 목록 페이지로 이동
+      document.location.href="/Portfolio"
+
     }
   }
 }
