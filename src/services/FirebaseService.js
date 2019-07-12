4ab2ac636ece4e5a441fc46f359886514d908107
Jinhwan Seo <dicmon@naver.com>
2019-07-12 14:27:14 +0900

    Add: email login function
    Using fire base service, now email login is possible.
    
    but unstable.
    
    #S1P1212010-16

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -112,5 +112,21 @@ export default {
 		});
 
 		  // [END createwithemail]
+	},
+	signIn(email, password){
+		firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// [START_EXCLUDE]
+			if (errorCode === 'auth/wrong-password') {
+				alert('Wrong password.');
+			} else {
+				alert(errorMessage);
+			}
+			console.log(error);
+			document.getElementById('quickstart-sign-in').disabled = false;
+			// [END_EXCLUDE]
+		});
 	}
 }
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -2,14 +2,56 @@
 	<v-layout align-center justify-center row fill-height>
 		<v-flex xs12 sm4 text-xs-center>
 			<div>
-				<v-btn round color="#df4a31" dark v-on:click="loginWithGoogle" style="width:100%;"><v-icon size="25" class="mr-2">fa-google</v-icon> Google 로그인</v-btn>
-			</div>
-			<div>
-				<v-btn round color="blue" dark v-on:click="loginWithFacebook" style="width:100%;"><v-icon size="25" class="mr-2">fa-facebook</v-icon> Facebook 로그인</v-btn>
+				<!-- SignIn modal -->
+				<v-dialog v-model="signInDialog" row justify-center max-width="500px">
+					<template v-slot:activator="{ on }">
+						<v-btn round color="#s2s2s2" dark style="width:100%;" v-on="on">SignIn</v-btn>
+					</template>
+					<v-card>
+						<v-card-title primary-title class="layout justify-center"> 
+							<h1>Sign In</h1>
+						</v-card-title>
+						<v-card-text>
+							<v-container grid-list-md>
+								<form @submit.prevent="">
+									<v-layout row>
+										<v-flex xs12>
+											<v-text-field
+												name='email'
+												label='Mail'
+												id='email'
+												v-model='email'
+												type='email'
+												required>
+											</v-text-field>
+										</v-flex>
+									</v-layout>
+									<v-layout row>
+										<v-flex xs12>
+											<v-text-field
+												name='password'
+												label='Password'
+												id='password'
+												v-model='password'
+												type='password'
+												required>
+											</v-text-field>
+										</v-flex>
+									</v-layout>
+									<v-card-actions class='mr-2'>
+										<v-spacer></v-spacer>
+										<v-btn color="blue darken-1" flat @click="signInDialog = false">Close</v-btn>
+										<v-btn color="blue darken-1" flat @click="signIn">Sign In</v-btn>
+									</v-card-actions>
+								</form>
+							</v-container>
+						</v-card-text>
+					</v-card>
+				</v-dialog>	  
 			</div>
 			<div>
 				<!-- Signup modal -->
-				<v-dialog v-model="dialog" row justify-center max-width="500px">
+				<v-dialog v-model="signUpDialog" row justify-center max-width="500px">
 					<template v-slot:activator="{ on }">
 						<v-btn round color="#s2s2s2" dark style="width:100%;" v-on="on">SignUp</v-btn>
 					</template>
@@ -58,8 +100,8 @@
 									</v-layout>
 									<v-card-actions class='mr-2'>
 										<v-spacer></v-spacer>
-										<v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
-										<v-btn color="blue darken-1" flat @click="signin">Save</v-btn>
+										<v-btn color="blue darken-1" flat @click="signUpDialog = false">Close</v-btn>
+										<v-btn color="blue darken-1" flat @click="signUp">Save</v-btn>
 									</v-card-actions>
 								</form>
 							</v-container>
@@ -67,6 +109,13 @@
 					</v-card>
 				</v-dialog>	  
 			</div>
+			<div>
+				<v-btn round color="#df4a31" dark v-on:click="loginWithGoogle" style="width:100%;"><v-icon size="25" class="mr-2">fa-google</v-icon> Google 로그인</v-btn>
+			</div>
+			<div>
+				<v-btn round color="blue" dark v-on:click="loginWithFacebook" style="width:100%;"><v-icon size="25" class="mr-2">fa-facebook</v-icon> Facebook 로그인</v-btn>
+			</div>
+			
 		</v-flex>
 
 	</v-layout>
@@ -81,7 +130,8 @@ export default {
 	store,	
 	data() {
 		return {
-			dialog: false,
+			signInDialog: false,
+			signUpDialog: false,
 			email: '',
             password: '',
             confirmPassword: '',
@@ -104,14 +154,17 @@ export default {
 		},
 		signUp: function(){
 			FirebaseService.signup(this.email, this.password)
-			this.dialog = false
+			this.signUpDialog = false
 		},
 		signIn: function(){
-			FirebaseService.signup(this.email, this.password)
+			
+			const result = FirebaseService.signIn(this.email, this.password)
+			console.log(result)
+			this.signInDialog = false
 			this.$router.replace('/')
 		}
 	},
-	mounted() {
+	mounted: function() {
 		console.log(this.$store.state)
 	},
 	computed: {
