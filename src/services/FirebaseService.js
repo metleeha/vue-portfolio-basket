08ac7af5bebf6d1bc571528f6f91e5b07405df08
Jinhwan Seo <dicmon@naver.com>
2019-07-12 13:57:23 +0900

    Add: Sign Up function
    Using firebase Service.
    
    now we can sign up with email

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -82,8 +82,35 @@ export default {
 			let user = result.user;
 			return result;
 			// ...
-		  }).catch(function(error) {
+		}).catch(function(error) {
 			console.error('[Facebook Login Error]', error)  
-		  });
+		});
+	},
+	signUp(email, password){
+		if (email.length < 4) {
+			alert('Please enter an email address.');
+			return;
+		}
+		if (password.length < 4) {
+			alert('Please enter a password.');
+			return;
+		}
+		  // Sign in with email and pass.
+		  // [START createwithemail]
+		firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// [START_EXCLUDE]
+			if (errorCode == 'auth/weak-password') {
+				alert('The password is too weak.');
+			} else {
+				alert(errorMessage);
+			}
+			console.log(error);
+			// [END_EXCLUDE]
+		});
+
+		  // [END createwithemail]
 	}
 }
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,74 +1,75 @@
 <template>
-  <v-layout align-center justify-center row fill-height>
-	  <v-flex xs12 sm4 text-xs-center>
-		  <div>
-			  <v-btn round color="#df4a31" dark v-on:click="loginWithGoogle" style="width:100%;"><v-icon size="25" class="mr-2">fa-google</v-icon> Google 로그인</v-btn>
-		  </div>
-		  <div>
-			  <v-btn round color="blue" dark v-on:click="loginWithFacebook" style="width:100%;"><v-icon size="25" class="mr-2">fa-facebook</v-icon> Facebook 로그인</v-btn>
-		  </div>
-		  <div>
-			<!-- Signup modal -->
-			<v-dialog v-model="dialog" row justify-center max-width="500px">
-				<template v-slot:activator="{ on }">
-					<v-btn round color="#s2s2s2" dark style="width:100%;" v-on="on">SignUp</v-btn>
-				</template>
-				<v-card>
-					<v-card-title primary-title class="layout justify-center"> 
-						<h1>Sign Up</h1>
-					</v-card-title>
-					<v-card-text>
-						<v-container grid-list-md>
-							 <form @submit.prevent="">
-								<v-layout row>
-									<v-flex xs12>
-										<v-text-field
-											name='email'
-											label='Mail'
-											id='email'
-											type='email'
-											required>
-										</v-text-field>
-									</v-flex>
-								</v-layout>
-								<v-layout row>
-									<v-flex xs12>
-										<v-text-field
-											name='password'
-											label='Password'
-											id='password'
-											v-model='password'
-											type='password'
-											required>
-										</v-text-field>
-									</v-flex>
-								</v-layout>
-								<v-layout row>
-									<v-flex xs12>
-										<v-text-field
-											name='confirmPassword'
-											label='Confirm Password'
-											id='confirmPassword'
-											v-model='confirmPassword'
-											type='password'
-											:rules="[comparePasswords]">
-										</v-text-field>
-									</v-flex>
-								</v-layout>
-								<v-card-actions class='mr-2'>
-									<v-spacer></v-spacer>
-									<v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
-									<v-btn color="blue darken-1" flat @click="dialog = false">Save</v-btn>
-								</v-card-actions>
-							</form>
-						</v-container>
-					</v-card-text>
-				</v-card>
-			</v-dialog>	  
-		  </div>
-	  </v-flex>
+	<v-layout align-center justify-center row fill-height>
+		<v-flex xs12 sm4 text-xs-center>
+			<div>
+				<v-btn round color="#df4a31" dark v-on:click="loginWithGoogle" style="width:100%;"><v-icon size="25" class="mr-2">fa-google</v-icon> Google 로그인</v-btn>
+			</div>
+			<div>
+				<v-btn round color="blue" dark v-on:click="loginWithFacebook" style="width:100%;"><v-icon size="25" class="mr-2">fa-facebook</v-icon> Facebook 로그인</v-btn>
+			</div>
+			<div>
+				<!-- Signup modal -->
+				<v-dialog v-model="dialog" row justify-center max-width="500px">
+					<template v-slot:activator="{ on }">
+						<v-btn round color="#s2s2s2" dark style="width:100%;" v-on="on">SignUp</v-btn>
+					</template>
+					<v-card>
+						<v-card-title primary-title class="layout justify-center"> 
+							<h1>Sign Up</h1>
+						</v-card-title>
+						<v-card-text>
+							<v-container grid-list-md>
+								<form @submit.prevent="">
+									<v-layout row>
+										<v-flex xs12>
+											<v-text-field
+												name='email'
+												label='Mail'
+												id='email'
+												v-model='email'
+												type='email'
+												required>
+											</v-text-field>
+										</v-flex>
+									</v-layout>
+									<v-layout row>
+										<v-flex xs12>
+											<v-text-field
+												name='password'
+												label='Password'
+												id='password'
+												v-model='password'
+												type='password'
+												required>
+											</v-text-field>
+										</v-flex>
+									</v-layout>
+									<v-layout row>
+										<v-flex xs12>
+											<v-text-field
+												name='confirmPassword'
+												label='Confirm Password'
+												id='confirmPassword'
+												v-model='confirmPassword'
+												type='password'
+												:rules="[comparePasswords]">
+											</v-text-field>
+										</v-flex>
+									</v-layout>
+									<v-card-actions class='mr-2'>
+										<v-spacer></v-spacer>
+										<v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
+										<v-btn color="blue darken-1" flat @click="signin">Save</v-btn>
+									</v-card-actions>
+								</form>
+							</v-container>
+						</v-card-text>
+					</v-card>
+				</v-dialog>	  
+			</div>
+		</v-flex>
 
-  </v-layout>
+	</v-layout>
 </template>
 
 <script>
@@ -77,11 +78,11 @@ import store from '../store'
 
 export default {
 	name: 'LoginPage',
-  	store,	
+	store,	
 	data() {
 		return {
 			dialog: false,
-			eamil: '',
+			email: '',
             password: '',
             confirmPassword: '',
 		}
@@ -101,9 +102,17 @@ export default {
 			this.$store.state.user = result.user
 			this.$router.replace('/')
 		},
+		signUp: function(){
+			FirebaseService.signup(this.email, this.password)
+			this.dialog = false
+		},
+		signIn: function(){
+			FirebaseService.signup(this.email, this.password)
+			this.$router.replace('/')
+		}
+	},
 	mounted() {
 		console.log(this.$store.state)
-		}
 	},
 	computed: {
 		comparePasswords () {
