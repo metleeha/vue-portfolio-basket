ac982635ccdced1d56c23ea6e9c5f4c0003b7845
metleeha <nodongth@gmail.com>
2019-07-16 18:02:33 +0900

    ING

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,52 +1,55 @@
 <template>
-<v-layout row justify-center>
-    <v-dialog v-model="dialog" max-width="400px">
-        <template v-slot:activator="{ on }">
-            <v-btn color="primary" dark v-on="on">Login</v-btn>
-        </template>
-        <!-- modal -->
-        <v-card>
-            <!-- header -->
-            <v-toolbar tabs color="secondary">
-                <v-toolbar-title><span class="headline">Login</span></v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-btn icon dark @click="dialog = false">
-                    <v-icon>close</v-icon>
-                </v-btn>
-            </v-toolbar>
+    <v-layout row justify-center>
+        <v-dialog v-model="dialog" max-width="400px">
+            <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark v-on="on">Login</v-btn>
+            </template>
+            <!-- modal -->
+            <v-card>
+                <!-- header -->
+                <v-toolbar tabs color="secondary">
+                    <v-toolbar-title><span class="headline">Login</span></v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-btn icon dark @click="dialog = false">
+                        <v-icon>close</v-icon>
+                    </v-btn>
+                </v-toolbar>
 
-            <!-- contents -->
-            <v-container grid-list-md>
-                <!-- Login with E-mail -->
+                <!-- contents -->
                 <v-form>
-                    <v-layout wrap row>
-						<v-flex>
-							<v-layout wrap>
-                            <!-- id&pw -->
-                            <v-text-field prepend-icon="face" label="E-mail" type="text" required></v-text-field>
-                            <v-text-field label="Password" type="password" required></v-text-field>
-                        </v-layout>
-                        <v-layout wrap row>
-                            <v-btn>Login</v-btn>
-                        </v-layout>
-                        <v-layout wrap row>
-                            <v-flex xs4><small>회원가입</small></v-flex>
-                            <v-flex xs4><small>ID&PW 찾기</small></v-flex>
-
-                        </v-layout>
-						</v-flex>
+                <v-container grid-list-md>
+                    <!-- Login with E-mail -->
+                    <v-layout row wrap>
                         
+                            <v-flex xs12 >
+                                <v-text-field class="please" label="E-mail" prepend-icon="face" type="text" required></v-text-field>
+                            </v-flex>
+                            <v-flex xs12>
+                                <v-text-field label="Password" prepend-icon="lock" type="password" required></v-text-field>
+                            </v-flex>
+
+                            <v-layout wrap row>
+                                <v-btn>Login</v-btn>
+                            </v-layout>
+
+                                
+                            <v-layout wrap row>
+                                <v-flex xs4><small>회원가입</small></v-flex>
+                                <v-flex xs4><small>ID&PW 찾기</small></v-flex>
+                            </v-layout>
+                                              
                     </v-layout>
 
-                </v-form>
-                <!-- Login without E-mail -->
-                <v-layout>
+                    <!-- Login without E-mail -->
+                    <v-layout>
 
-                </v-layout>
-            </v-container>
-        </v-card>
-    </v-dialog>
-</v-layout>
+                    </v-layout>
+                </v-container>
+                </v-form>
+            </v-card>
+            <!-- end modal -->
+        </v-dialog>
+    </v-layout>
 </template>
 
 <script>
@@ -110,3 +113,8 @@ export default {
     }
 }
 </script>
+<style scoped>
+.please{
+    display: inline-flex;
+}
+</style>
