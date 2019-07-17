14e88cc89885de11984a275c2d1f5338ec35baee
Jinhwan Seo <dicmon@naver.com>
2019-07-17 11:51:49 +0900

    Add: Login Modal
    #S1P1212010-71

--- src/components/LoginDialog.vue
+++ src/components/LoginDialog.vue
@@ -1,62 +1,74 @@
 <template>
-<v-layout row justify-center>
-      <v-dialog v-model="loginDialog" persistent max-width="600px">
-        <template v-slot:activator="{ on }">
-          <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="headline">User Profile</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field label="Legal first name*" required></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field
-                    label="Legal last name*"
-                    hint="example of persistent helper text"
-                    persistent-hint
-                    required
-                  ></v-text-field>
-                </v-flex>
-                <v-flex xs12>
-                  <v-text-field label="Email*" required></v-text-field>
-                </v-flex>
-                <v-flex xs12>
-                  <v-text-field label="Password*" type="password" required></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6>
-                  <v-select
-                    :items="['0-17', '18-29', '30-54', '54+']"
-                    label="Age*"
-                    required
-                  ></v-select>
-                </v-flex>
-                <v-flex xs12 sm6>
-                  <v-autocomplete
-                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                    label="Interests"
-                    multiple
-                  ></v-autocomplete>
-                </v-flex>
-              </v-layout>
-            </v-container>
-            <small>*indicates required field</small>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
-            <v-btn color="blue darken-1" flat @click="dialog = false">Save</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-layout>
+  <v-layout align-center justify-center row fill-height>
+	  <v-flex xs12 sm4 text-xs-center>
+		  <div>
+			  <v-btn round color="#df4a31" dark v-on:click="loginWithGoogle" style="width:100%;"><v-icon size="25" class="mr-2">fa-google</v-icon> Google 로그인</v-btn>
+		  </div>
+		  <div>
+			  <v-btn round color="blue" dark v-on:click="loginWithFacebook" style="width:100%;"><v-icon size="25" class="mr-2">fa-facebook</v-icon> Facebook 로그인</v-btn>
+		  </div>
+		  <div>
+			<!-- Signup modal -->
+			<v-dialog v-model="dialog" row justify-center max-width="500px">
+				<template v-slot:activator="{ on }">
+					<v-btn round color="#s2s2s2" dark style="width:100%;" v-on="on">SignUp</v-btn>
+				</template>
+				<v-card>
+					<v-card-title primary-title class="layout justify-center"> 
+						<h1>Sign Up</h1>
+					</v-card-title>
+					<v-card-text>
+						<v-container grid-list-md>
+							 <form @submit.prevent="">
+								<v-layout row>
+									<v-flex xs12>
+										<v-text-field
+											name='email'
+											label='Mail'
+											id='email'
+											type='email'
+											required>
+										</v-text-field>
+									</v-flex>
+								</v-layout>
+								<v-layout row>
+									<v-flex xs12>
+										<v-text-field
+											name='password'
+											label='Password'
+											id='password'
+											v-model='password'
+											type='password'
+											required>
+										</v-text-field>
+									</v-flex>
+								</v-layout>
+								<v-layout row>
+									<v-flex xs12>
+										<v-text-field
+											name='confirmPassword'
+											label='Confirm Password'
+											id='confirmPassword'
+											v-model='confirmPassword'
+											type='password'
+											:rules="[comparePasswords]">
+										</v-text-field>
+									</v-flex>
+								</v-layout>
+								<v-card-actions class='mr-2'>
+									<v-spacer></v-spacer>
+									<v-btn color="blue darken-1" flat @click="dialog = false">Close</v-btn>
+									<v-btn color="blue darken-1" flat @click="dialog = false">Save</v-btn>
+								</v-card-actions>
+							</form>
+						</v-container>
+					</v-card-text>
+				</v-card>
+			</v-dialog>	  
+		  </div>
+	  </v-flex>
+
+  </v-layout>
 </template>
 
 <script>
@@ -65,12 +77,11 @@ import store from '../store'
 
 export default {
 	name: 'LoginPage',
-	store,	
+  	store,	
 	data() {
 		return {
-			signInDialog: false,
-			signUpDialog: false,
-			email: '',
+			dialog: false,
+			eamil: '',
             password: '',
             confirmPassword: '',
 		}
@@ -90,28 +101,9 @@ export default {
 			this.$store.state.user = result.user
 			this.$router.replace('/')
 		},
-		async signUp(){
-			const result = await FirebaseService.signUp(this.email, this.password)
-			if(result){
-				alert("가입완료!");
-				this.email = "";
-				this.password = "";
-				this.confirmPassword = "";
-				this.signUpDialog = false;
-			}
-		},
-		async signIn(){
-			const result = await FirebaseService.signIn(this.email, this.password)
-			if(result){
-				alert("로그인!");
-				this.$store.state.accessToken = result.accessToken;
-				this.$store.state.user = result.user;
-				this.$router.replace('/');
-			}
-		}
-	},
-	mounted: function() {
+	mounted() {
 		console.log(this.$store.state)
+		}
 	},
 	computed: {
 		comparePasswords () {
