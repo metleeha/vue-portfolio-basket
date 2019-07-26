711dd130b4caec62bd3b90fcace022e3b828c1c5
Jinhwan Seo <dicmon@naver.com>
2019-07-26 10:51:16 +0900

    Add: pwa enable option
    #S1P1213010-9

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -10,6 +10,21 @@ const BANNERIMAGE = 'bannerimage'
 const config = require("../../ignore/firebaseAPI.json");
 
 firebase.initializeApp(config)
+
+/* Firebase PWA enable */
+firebase.firestore().enablePersistence()
+  .catch(function(err) {
+      if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  });
+
 const firestore = firebase.firestore()
 
 export default {
