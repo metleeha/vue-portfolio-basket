27d7d844c11baf26ae4ec9266ff36a6675f62d85
정우성 <nisroeld91@gmail.com>
2019-07-29 14:22:29 +0900

    Add: Post page
    #S1P1213010-39

--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <ImgBanner imgSrc="https://source.unsplash.com/5brvJbR1Pn8/1600x900">
+    <ImgBanner>
       <div style="line-height:1.2em;font-size:1.2em;" slot="text">Post</div>
     </ImgBanner>
     <v-container>
-
+      <!-- Add New Post Button -->
+      <v-layout xs12 justify-center>
+        <v-flex xs12 text-xs-center mt-5>
+          <v-btn @click="newToggle = !newToggle" block flat large :color="newToggle? '#f0bebe':'#3a718c'"><v-icon left>fa-chevron-down</v-icon>New Post</v-btn>
+        </v-flex>
+      </v-layout>
+      <!-- Post Writer --->
+      <transition name="fade">
+        <v-layout v-if="newToggle" xs12 justify-center>
+          <PostWriter />
+        </v-layout>
+      </transition>
       <!-- Post -->
       <v-layout>
         <v-flex xs12>
@@ -19,12 +30,28 @@
 <script>
 import ImgBanner from '../components/ImgBanner'
 import PostList from '../components/PostList'
+import PostWriter from '../components/PostWriter'
 
 export default {
 	name: 'PostPage',
 	components: {
 		ImgBanner,
-		PostList,
-	}
+    PostList,
+    PostWriter
+	},
+  data(){
+    return {
+      newToggle: false,
+    }
+  }
 }
 </script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
