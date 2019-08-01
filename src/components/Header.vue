facce0460c9167563bced32e959a426244501e52
metleeha <nodongth@gmail.com>
2019-08-01 14:24:38 +0900

    Fix: double sidebar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,5 @@
 <template>
 <div>
-    <!-- sidebar -->
-    <v-navigation-drawer v-model="drawer" app fixed temporary>
-        <v-list>
-            <v-container justify-center>
-                <v-btn flat to="/" style="cursor: pointer">
-                    <v-icon class="mr-1">free_breakfast</v-icon>TEN</v-btn>
-                <v-flex>
-                    <v-btn flat to="portfolio" style="cursor: pointer">
-                        <v-icon left>assessment</v-icon>Portfolio</v-btn>
-                </v-flex>
-                <v-flex>
-                    <v-btn flat to="post" style="cursor: pointer">
-                        <v-icon left>assessment</v-icon>Post</v-btn>
-                </v-flex>
-                <v-flex>
-                    <LoginDialog></LoginDialog>
-                </v-flex>
-            </v-container>
-        </v-list>
-    </v-navigation-drawer>
-    <!-- end sidebar -->
-
-    <!-- #112d4e-->
     <!-- navbar -->
     <v-toolbar color='#248ea9' fixed app temporary dark scroll-off-screen>
         <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up">
@@ -58,6 +35,10 @@
                     <v-btn flat to="portfolio" style="cursor: pointer">
                         <v-icon left>assessment</v-icon>Portfolio
                     </v-btn>
+                </v-flex>
+                <v-flex>
+                    <v-btn flat to="post" style="cursor: pointer">
+                        <v-icon left>assessment</v-icon>Post</v-btn>
                 </v-flex>
                 <v-flex v-show="!this.isSignined">
                     <SignInMenu></SignInMenu>
