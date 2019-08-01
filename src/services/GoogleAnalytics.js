c73e55ac92a4c80e9978f20dec95ef121606393e
metleeha <nodongth@gmail.com>
2019-08-01 15:34:07 +0900

    Fix: Google Analaytics Key
    #S1P1213010-61

---
+++ src/services/GoogleAnalytics.js
@@ -0,0 +1,9 @@
+ //<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144465306-1"></script>
+ // Google Analytics 
+    window.dataLayer = window.dataLayer || [];
+
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'UA-xx');
